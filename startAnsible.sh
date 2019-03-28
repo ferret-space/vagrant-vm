9b6ecc891a04c93eedf3c0f5008c5c5853347734
Denis Pingin <denis.pingin@adesso.ch>
2019-03-28 14:22:33 +0100

    install maven and npm

--- startAnsible.sh
+++ startAnsible.sh
@@ -4,6 +4,6 @@ cd /home/vagrant
 mkdir projects
 cd projects
 git clone https://git.adesso.ch/denis.pingin/demo-vm.git
-cd vm
+cd demo-vm
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbook.yml
